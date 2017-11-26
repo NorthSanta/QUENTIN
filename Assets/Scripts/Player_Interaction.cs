d2f9a947b0f92704e8ee549ca4447a31df8bc4f0
NorthSanta <xavidevesa@hotmail.com>
2017-11-26 14:40:06 +0100

    icons

--- Assets/Scripts/Player_Interaction.cs
+++ Assets/Scripts/Player_Interaction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PostProcessing;
+using UnityEngine.UI;
 
 public class Player_Interaction : MonoBehaviour {
     RaycastHit interact;
@@ -14,33 +15,42 @@ public class Player_Interaction : MonoBehaviour {
     public GameObject panel;
     public Transform test;
     public Texture2D mouse;
-   // public Object_Movement move;
+    public Image icon;
+    public Sprite lupa;
+    public Sprite quadrat;
+    private GameObject myCanvas;
+    // public Object_Movement move;
     // Use this for initialization
     void Start () {
         count = 0;
         picked = false;
         ppProfile.depthOfField.enabled = false;
         ppProfile.vignette.enabled = false;
+        myCanvas = GameObject.Find("Canvas");
+        Cursor.visible = false;
         //ppProfile = Camera.main.GetComponent<PostProcessingProfile>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        icon.sprite = quadrat;
         Debug.DrawRay(transform.position,transform.forward *1.5f,Color.green);
         
             if (Physics.Raycast(transform.position, transform.forward, out interact, 1.5f) && !picked)
             {
-                if(interact.collider.tag == "Interact")
+                /*if(interact.collider.tag == "Interact")
                 {
+                    icon.sprite = lupa;
                     print("PICK");
-                }
+                }*/
                 if ((interact.collider.tag != "Paret") && (interact.collider.tag != "Terra"))
                 {
+                    icon.sprite = lupa;
                     canPick = true;
                 }
                 else
                 {
-
+                   
                     canPick = false;
                 }
 
@@ -55,23 +65,24 @@ public class Player_Interaction : MonoBehaviour {
 
         if (canPick && Input.GetKeyDown(KeyCode.E) && !picked)
         {
+           
             ppProfile.depthOfField.enabled = true;
             ppProfile.vignette.enabled = true;
+
             //Camera.main.GetComponent<PostProcessingProfile>().depthOfField.settings = ppProfile.depthOfField.settings;
-            Cursor.SetCursor(mouse, Vector2.zero, CursorMode.Auto);
+            //Cursor.SetCursor(mouse, Vector2.zero, CursorMode.Auto);
             Cursor.lockState = CursorLockMode.None;
-           // panel.SetActive(true);
-            //panel.transform.localPosition = new Vector3(0, 0, 500);
-            //panel.transform.localScale = new Vector3(10,10,10);
+           
             vell = interact.collider.gameObject;
             vell.SetActive(false);
             nou = (GameObject)Instantiate(interact.collider.gameObject);
             nou.SetActive(true);
             nou.layer = 5;
-            //transform.rotation = new Quaternion(0, 0, 0, 0);
+            
             nou.GetComponent<BoxCollider>().enabled = false;
-            //Destroy(nou.GetComponent<Rigidbody>());
-            nou.transform.parent = GameObject.Find("Canvas").transform;
+
+            nou.transform.parent =myCanvas.transform;
+            nou.transform.SetAsFirstSibling();
             nou.transform.localPosition = new Vector3(0,0,0);
             nou.transform.rotation = new Quaternion(0, 0, 0, 0);
             nou.AddComponent<Object_Movement>();
@@ -81,6 +92,7 @@ public class Player_Interaction : MonoBehaviour {
         }
         else if(picked && Input.GetKeyDown(KeyCode.E) && picked)
         {
+            
             ppProfile.depthOfField.enabled = false;
             ppProfile.vignette.enabled = false;
             Cursor.lockState = CursorLockMode.Locked;
@@ -88,6 +100,13 @@ public class Player_Interaction : MonoBehaviour {
             vell.SetActive(true);
             Destroy(nou);
             picked = false;
+        }
+
+        if (picked)
+        {
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, Camera.main, out pos);
+            icon.transform.position = myCanvas.transform.TransformPoint(pos);
         }
         
     }
