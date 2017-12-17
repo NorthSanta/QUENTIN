d1e01ecf1ef37fe85786ccfdcc2a9a77299edaef
MarcKami <Marc.kami1991@gmail.com>
2017-12-17 21:14:17 +0100

    ScanDetection

--- Assets/Scripts/Scanner/Scannable.cs
+++ Assets/Scripts/Scanner/Scannable.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Scannable : MonoBehaviour
-{
-	public Animator UIAnim;
+public class Scannable : MonoBehaviour {
+    //public Animator UIAnim;
+    //S'han de posar els escanejables dins de un EmptyObject per separat cadascun
+    public Transform[] active;
 
-	public void Ping()
-	{
-		UIAnim.SetTrigger("Ping");
+    public void Ping() {
+        //UIAnim.SetTrigger("Ping");
+        active = transform.GetComponentsInChildren<Transform>(true);
+
+        active[1].gameObject.SetActive(true);
     }
 }
