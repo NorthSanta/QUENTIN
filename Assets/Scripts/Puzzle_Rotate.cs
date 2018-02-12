c4a91046967114a3132f1930e69605cc4ee9f05c
NorthSanta <devesa1996@gmail.com>
2018-02-12 20:26:49 +0100

    puzzle Rotation

---
+++ Assets/Scripts/Puzzle_Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Rotate : MonoBehaviour {
+
+    public float smooth = 1f;
+    private Quaternion targetRotation;
+    void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            targetRotation *= Quaternion.AngleAxis(-90, Vector3.forward);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+    }
+}
