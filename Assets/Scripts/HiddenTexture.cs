9485c7ef7d47f027a64f26c347271558bf145a2e
NorthSanta <devesa1996@hotmail.com>
2018-03-01 13:18:49 +0100

    Revealing Light First Try

---
+++ Assets/Scripts/HiddenTexture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenTexture : MonoBehaviour
+{
+
+    Transform tfLight;
+
+    private void Start()
+    {
+        // find the revealing light named "RevealingLight":
+        GameObject goLight = GameObject.Find("RevealingLight");
+        if (goLight) tfLight = goLight.transform;
+    }
+
+
+    private void Update()
+    {
+        if (tfLight)
+        {
+            GetComponent<Renderer>().material.SetVector("_LightPos", tfLight.position);
+            GetComponent<Renderer>().material.SetVector("_LightDir", tfLight.forward);
+        }
+    }
+}
+
+
