721ec375f496a0b709ecc796f26c4576bfd75fa8
MarcKami <Marc.kami1991@gmail.com>
2018-03-05 17:59:34 +0100

    ClockUpdate

---
+++ Assets/Scripts/StudioClock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StudioClock : MonoBehaviour {
+
+    [SerializeField] private TextMeshPro[] hours;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        System.DateTime date1 = System.DateTime.Now;
+        string final = (date1.ToString("%H") + ":" + date1.ToString("%m"));
+        for (int i = 0; i < 8; i++) {
+            hours[i].SetText(final);
+        }
+    }
+}
