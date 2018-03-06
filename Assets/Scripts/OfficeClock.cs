641797d35b01aa37abaebe67d6c6c5c5acd7d707
MarcKami <Marc.kami1991@gmail.com>
2018-03-06 11:08:14 +0100

    Improves On Office

---
+++ Assets/Scripts/OfficeClock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OfficeClock : MonoBehaviour {
+
+    [SerializeField] private TextMeshPro hours;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.DateTime date1 = System.DateTime.Now;
+        string finalDate = "06/30/2035 ";
+        string finalH = (date1.ToString("%HH") + ":" + date1.ToString("%mm"));
+        hours.SetText(finalDate + finalH);
+    }
+}
